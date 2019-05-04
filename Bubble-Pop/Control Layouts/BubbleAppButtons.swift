637522577ee42d71cf59319455cc5c798459a935
anltnmse60906 <anltnmse60906@fpt.edu.vn>
2019-05-04 18:34:31 +1000

    Change layout of the application

---
+++ Bubble-Pop/Control Layouts/BubbleAppButtons.swift
@@ -0,0 +1,31 @@
+//
+//  BubbleAppButton.swift
+//  Bubble-Pop
+//
+//  Created by Tran Nhut Minh An Le on 4/5/19.
+//  Copyright © 2019 Tran Nhut Minh An Le. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
